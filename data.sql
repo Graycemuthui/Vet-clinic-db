064099c5b25760fdbb804fbf495351a5ecb5ab09
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-09 20:15:08 +0300

    Add row

--- data.sql
+++ data.sql
@@ -3,3 +3,4 @@
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', date '2020-02-03', 0 , TRUE , 10.23);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', date '2021-01-07', 1, FALSE, 15.04);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', date '2017-04-12', 5, TRUE, 11);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018/11/15', '2', 'true', '8.0');
