3723e70bf1ee957d7d94c94ae0d23c0d0425d596
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-09 18:00:04 +0300

    Insert data and create queries

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT,
     neutered BOOLEAN,
-    weight_kg DEC NOT NULL, 
+    weight_kg DECIMAL NOT NULL,
+    PRIMARY KEY(id)
 );
