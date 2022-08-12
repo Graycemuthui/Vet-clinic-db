22891fcc29418ca34d3a71f1afd79228a906edf4
Grayce Muthui <graycemuthui20@gmail.com>
2022-08-12 15:48:07 +0300

    Insert rows in all tables

--- schema.sql
+++ schema.sql
@@ -36,3 +36,27 @@ ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 -- Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
 -- animals_owners_id_fkey" FOREIGN KEY (owners_id) REFERENCES owners(id)
+
+
+-- JOIN TABLES
+-- Create the vets to store vets.
+CREATE TABLE vets(
+    id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+    name VARCHAR NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE,
+    PRIMARY KEY (id)
+);
+
+-- Create a join table to store species and vets.
+CREATE TABLE specialization(
+    species_id INT REFERENCES species(id),
+    vet_id INT REFERENCES vets(id)
+);
+
+-- Create a join table to store animals and vets.
+CREATE TABLE visits(
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    date_of_visit DATE
+);
